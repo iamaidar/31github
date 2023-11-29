9fcb3e4964da49217fb48100bbb6d96ed30da472
iamaidar <aydarmaratov17@gmail.com>
2023-11-29 14:39:01 +0600

    Number of 1 Bits

---
+++ hamingWeight.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func hammingWeight(num uint32) int {
+	count := 0
+	for _, val := range []rune(fmt.Sprintf("%b", num)) {
+		if val == []rune("1")[0] {
+			count++
+		}
+	}
+	return count
+}
