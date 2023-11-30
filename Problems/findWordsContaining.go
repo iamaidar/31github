60d6248f0433d365f21f387c449926c50314dc1a
iamaidar <aydarmaratov17@gmail.com>
2023-11-30 23:16:37 +0600

    New solution

---
+++ Problems/findWordsContaining.go
@@ -0,0 +1,16 @@
+func findWordsContaining(words []string, x byte) []int {
+	var res []int
+
+	for idx, word := range words {
+		var check bool
+		for _, val := range []rune(word) {
+			if []rune(string(x))[0] == val {
+				check = true
+			}
+		}
+		if check {
+			res = append(res, idx)
+		}
+	}
+	return res
+}
