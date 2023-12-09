b93846e3d9f56fa40604ca4f15d907cc29bb8efe
iamaidar <aydarmaratov17@gmail.com>
2023-12-09 18:42:48 +0600

    Solution: 09/12/23

---
+++ Problems/inorderTraversal.go
@@ -0,0 +1,27 @@
+package problems
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func inorderTraversal(root *TreeNode) []int {
+	var res []int
+	stack := []*TreeNode{}
+
+	for root != nil || len(stack) > 0 {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+
+		root = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		res = append(res, root.Val)
+		root = root.Right
+	}
+
+	return res
+}
