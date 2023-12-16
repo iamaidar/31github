ccf213f81f9c9ea7460b0636cdc1b1bfbd05ce9b
iamaidar <aydarmaratov17@gmail.com>
2023-12-16 17:43:06 +0600

    Solution: 16/12/23

--- Problems/isAnagram.go
+++ Problems/isAnagram.go
@@ -10,6 +10,9 @@ func isAnagram(s string, t string) bool {
 	}
 
 	for i := 0; i < len(t); i++ {
+		if _, ok := m[t[i]]; !ok {
+			return false
+		}
 		m[t[i]]--
 		if m[t[i]] < 0 {
 			return false
@@ -18,3 +21,22 @@ func isAnagram(s string, t string) bool {
 
 	return true
 }
+
+// func isAnagram(s string, t string) bool {
+// 	if len(s) != len(t) {
+// 		return false
+// 	}
+// 	m := make(map[byte]int)
+// 	for i := 0; i < len(s); i++ {
+// 		m[s[i]]++
+// 	}
+
+// 	for i := 0; i < len(t); i++ {
+// 		m[t[i]]--
+// 		if m[t[i]] < 0 {
+// 			return false
+// 		}
+// 	}
+
+// 	return true
+// }
