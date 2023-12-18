58bbfed760d8803afd3c2a7a9f04e89c924a05f8
iamaidar <aydarmaratov17@gmail.com>
2023-12-18 23:48:03 +0600

    Solution: 18/12/23

--- Problems/maxProductDifference.go
+++ Problems/maxProductDifference.go
@@ -1,22 +1,23 @@
 package problems
 
-m1, m2, s1, s2 := 0, 0, 99999999999999, 99999999999999
+func maxProductDifference(nums []int) int {
+	m1, m2, s1, s2 := 0, 0, 99999999999999, 99999999999999
 
- for i := 0; i < len(nums); i++ {
-  if nums[i] > m1 {
-   m2 = m1
-   m1 = nums[i]
-  } else if nums[i] > m2 {
-   m2 = nums[i]
-  }
+	for i := 0; i < len(nums); i++ {
+		if nums[i] > m1 {
+			m2 = m1
+			m1 = nums[i]
+		} else if nums[i] > m2 {
+			m2 = nums[i]
+		}
 
-  if nums[i] < s1 {
-   s2 = s1
-   s1 = nums[i]
-  } else if nums[i] < s2 {
-   s2 = nums[i]
-  }
- }
+		if nums[i] < s1 {
+			s2 = s1
+			s1 = nums[i]
+		} else if nums[i] < s2 {
+			s2 = nums[i]
+		}
+	}
 
-    return m1*m2 - s1*s2 
+	return m1*m2 - s1*s2
 }
