6704ce6f77ec46d829aaf6be81932e20f34a5f4b
iamaidar <aydarmaratov17@gmail.com>
2023-12-19 23:20:23 +0600

    Solution: 19/12/23

--- Problems/imageSmoother.go
+++ Problems/imageSmoother.go
@@ -24,3 +24,67 @@ func imageSmoother(img [][]int) [][]int {
 
 	return res
 }
+
+/*
+func imageSmoother(img [][]int) [][]int {
+    var smooth [][]int = make([][]int, len(img))
+    for i := 0; i<len(img); i++{
+        smooth[i] = make([]int, len(img[0]))
+    }
+    l := len(img)    //length
+    w := len(img[0])    //width
+    for i := 0; i < l; i++ {
+        for j := 0; j < w ; j++{
+            sum := 0
+            n := 0
+            //above left
+            if i > 0 && j > 0{
+                n+=1
+                sum += img[i-1][j-1]
+            }
+            //above
+            if i > 0 {
+                n+=1
+                sum += img[i-1][j]
+            }
+            //above right
+            if i > 0 && j < w-1{
+                n+=1
+                sum += img[i-1][j+1]
+            }
+
+            // left
+            if j > 0{
+                n+=1
+                sum += img[i][j-1]
+            }
+            //self
+            n+=1
+            sum += img[i][j]
+            // right
+            if j < w-1{
+                n+=1
+                sum += img[i][j+1]
+            }
+            // below left
+            if i < l-1 && j > 0{
+                n+=1
+                sum += img[i+1][j-1]
+            }
+            //below
+            if i < l-1 {
+                n+=1
+                sum += img[i+1][j]
+            }
+            //below right
+            if i < l-1 && j < w-1{
+                n+=1
+                sum += img[i+1][j+1]
+            }
+            //calculate avg
+            smooth[i][j] = sum/n
+        }
+    }
+    return smooth
+}
+*/
