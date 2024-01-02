c06e0a0f60cdbf44cb352b608104405e35ca8188
iamaidar <aydarmaratov17@gmail.com>
2024-01-02 15:16:49 +0600

    Solution: 02/01/24

--- Problems/findMatrix.go
+++ Problems/findMatrix.go
@@ -1,28 +1,46 @@
 package problems
 
+//BEST SOLUTION
 func findMatrix(nums []int) [][]int {
 	var res [][]int
 	m := make(map[int]int)
-	max := 0
 
 	for _, val := range nums {
+		if m[val] >= len(res) {
+			res = append(res, []int{})
+		}
+
+		res[m[val]] = append(res[m[val]], val)
 		m[val]++
-		if m[val] > max {
-			max = m[val]
-		}
-	}
-
-	for i := 0; i < max; i++ {
-		var temp []int
-		for k, val := range m {
-			if val != 0 {
-				m[k]--
-				temp = append(temp, k)
-			}
-		}
-
-		res = append(res, temp)
 	}
 
 	return res
 }
+
+//MY SOLUTION
+// func findMatrix(nums []int) [][]int {
+// 	var res [][]int
+// 	m := make(map[int]int)
+// 	max := 0
+
+// 	for _, val := range nums {
+// 		m[val]++
+// 		if m[val] > max {
+// 			max = m[val]
+// 		}
+// 	}
+//
+// 	for i := 0; i < max; i++ {
+// 		var temp []int
+// 		for k, val := range m {
+// 			if val != 0 {
+// 				m[k]--
+// 				temp = append(temp, k)
+// 			}
+// 		}
+
+// 		res = append(res, temp)
+// 	}
+
+// 	return res
+// }
