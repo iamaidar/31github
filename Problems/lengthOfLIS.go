dd3cfe7e038e2dba8346f984eec6676a30ce4042
iamaidar <aydarmaratov17@gmail.com>
2024-01-06 00:54:26 +0600

    Solution: 06/01/24

---
+++ Problems/lengthOfLIS.go
@@ -0,0 +1,18 @@
+package problems
+
+func lengthOfLIS(nums []int) int {
+	temp := make([]int, len(nums))
+	m := 0
+
+	for i := 0; i < len(nums); i++ {
+		for j := 0; j < i; j++ {
+			if nums[j] < nums[i] {
+				temp[i] = max(temp[i], temp[j]+1)
+			}
+		}
+
+		m = max(temp[i], m)
+	}
+
+	return m + 1
+}
