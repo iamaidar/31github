ece0abf1bada84bbcda3c24f2fc60c09c6a5a51a
iamaidar <aydarmaratov17@gmail.com>
2024-01-16 12:33:10 +0600

    Solution: 16/01/24

--- Problems/RandomizedSet.go
+++ Problems/RandomizedSet.go
@@ -2,15 +2,20 @@ package problems
 
 import "math/rand"
 
-type RandomizedSet map[int]struct{}
+//BEST SOLUTION (HALF MY)
+type RandomizedSet struct {
+	m map[int]struct{}
+	n []int
+}
 
 func Constructor() RandomizedSet {
-	return RandomizedSet{}
+	return RandomizedSet{map[int]struct{}{}, []int{}}
 }
 
 func (this *RandomizedSet) Insert(val int) bool {
-	if _, ok := (*this)[val]; !ok {
-		(*this)[val] = struct{}{}
+	if _, ok := this.m[val]; !ok {
+		this.m[val] = struct{}{}
+		this.n = append(this.n, val)
 		return true
 	}
 
@@ -18,25 +23,57 @@ func (this *RandomizedSet) Insert(val int) bool {
 }
 
 func (this *RandomizedSet) Remove(val int) bool {
-	if _, ok := (*this)[val]; ok {
-		delete((*this), val)
-		return true
+	for i, v := range this.n {
+		if v == val {
+			delete(this.m, val)
+			this.n = append(this.n[:i], this.n[i+1:]...)
+			return true
+		}
 	}
 
 	return false
 }
 
 func (this *RandomizedSet) GetRandom() int {
-	i := rand.Intn(len(*this))
-	idx := 0
-	for k, _ := range *this {
-		if idx == i {
-			return k
-		}
-		idx++
-	}
-	return i
+	return this.n[rand.Intn(len(this.n))]
 }
+
+//MY SOLUTION (NON OPTIMIZED)
+// type RandomizedSet map[int]struct{}
+
+// func Constructor() RandomizedSet {
+// 	return RandomizedSet{}
+// }
+
+// func (this *RandomizedSet) Insert(val int) bool {
+// 	if _, ok := (*this)[val]; !ok {
+// 		(*this)[val] = struct{}{}
+// 		return true
+// 	}
+
+// 	return false
+// }
+
+// func (this *RandomizedSet) Remove(val int) bool {
+// 	if _, ok := (*this)[val]; ok {
+// 		delete((*this), val)
+// 		return true
+// 	}
+
+// 	return false
+// }
+
+// func (this *RandomizedSet) GetRandom() int {
+// 	i := rand.Intn(len(*this))
+// 	idx := 0
+// 	for k, _ := range *this {
+// 		if idx == i {
+// 			return k
+// 		}
+// 		idx++
+// 	}
+// 	return i
+// }
 
 /**
  * Your RandomizedSet object will be instantiated and called as such:
